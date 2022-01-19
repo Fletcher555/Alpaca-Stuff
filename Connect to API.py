adf384b9d8838db1e10ec11481c8995e0c1f9c0f
Fletcher555 <52580867+Fletcher555@users.noreply.github.com>
2022-01-18 19:33:59 -0700

    added Pycharm

---
+++ Connect to API.py
@@ -0,0 +1,10 @@
+import alpaca_trade_api as tradeapi
+
+class Alpaca(object):
+    def __init__(self):
+        self.key = 'PK2VQWH7X8DAC2LRTZIN'
+        self.secret = 'pGe1Wx0t8K4rZXbFelRMDm8sMHTQdtNvWtNCQqHw'
+        self.alpaca_endpoint = 'https://paper-api.alpaca.markets'
+        self.api = tradeapi.REST(self.key, self.secret, self.alpaca_endpoint)
+
+
---
+++ Model.py
@@ -0,0 +1,33 @@
+# First attempt to make black-scholes formula
+
+import numpy as numpy
+from scipy.stats import norm
+import Alpaca
+
+Alpaca = Alpaca()
+# Defining variables
+
+#r = float(input("Risk free interest rates: "))
+s = float(input("Stock price: "))
+k = float(input("Market strike price: "))
+t = float(input("Time of option expiration in years days: "))
+sigma = float(input("Sigma of stock: "))
+t=t/365
+sigma=sigma/100
+primeInterest = 2.5
+r=primeInterest/100
+
+def blackScholes(r, s, k, t, sigma, type="c"):
+    d1 = (numpy.log(s/k) + (r + sigma**2/2)*t)/(sigma*numpy.sqrt(t))
+    d2 = d1 - sigma*numpy.sqrt(t)
+    try:
+        if type == "c":
+            price = s*norm.cdf(d1, 0, 1) - k*numpy.exp(-r*t)*norm.cdf(d2, 0, 1)
+        elif type == "p":
+            price = k*numpy.exp(-r*t)*norm.cdf(-d2, 0, 1) - s*norm.cdf(-d1, 0, 1)
+        return price
+    except:
+        print("Error - Make sure all option parameters are correct ")
+
+print("Theoretical Option price is: ", round(blackScholes(r, s, k, t, sigma, type="c"),3))
+
