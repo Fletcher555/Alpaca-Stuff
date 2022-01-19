fc946b89cca12178fc215b3018008fc47d186910
Danie Larkin <98000682+sDanielLarkin@users.noreply.github.com>
2022-01-18 20:11:59 -0700

    update api

--- Connect to API.py
+++ Connect to API.py
@@ -2,8 +2,8 @@ import alpaca_trade_api as tradeapi
 
 class Alpaca(object):
     def __init__(self):
-        self.key = 'PK2VQWH7X8DAC2LRTZIN'
-        self.secret = 'pGe1Wx0t8K4rZXbFelRMDm8sMHTQdtNvWtNCQqHw'
+        self.key = 'PKQFCYN1MCXGCSRDCKYH'
+        self.secret = 'RJR8rq4Wo6pP5EzM3ABURAFX1C7TYPucnFjsq5pR'
         self.alpaca_endpoint = 'https://paper-api.alpaca.markets'
         self.api = tradeapi.REST(self.key, self.secret, self.alpaca_endpoint)
 
